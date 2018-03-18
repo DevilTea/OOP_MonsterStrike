cccaa0eed161e8befd0d84bf79ae88a04c5eb021
Xu-Jia <a5852241@gmail.com>
2018-03-19 02:21:54 +0800

    + Update Marble.js

--- src/js/Marble.js
+++ src/js/Marble.js
@@ -1,5 +1,13 @@
 class Marble extends MapObject {
 	constructor() {
 		super()
+		this.attribute = 0			//屬性
+		this.rebound = false		//反射/貫通
+		this.hp = 0					//HP
+		this.atk = 0				//攻擊力
+		this.spd = 0				//速度
+		this.race = 0				//種族
+		this.skill = []				//技能
+		this.comboSkill = []		//友情技能
 	}
 }
