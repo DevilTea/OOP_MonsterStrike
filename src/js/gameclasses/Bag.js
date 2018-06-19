a584ee951c40e94ee3953da268befe7f60067035
zxjte9411 <a5852241@gmail.com>
2018-06-20 04:53:03 +0800

    修改預設背包

--- src/js/gameclasses/Bag.js
+++ src/js/gameclasses/Bag.js
@@ -6,19 +6,5 @@ GameClasses.Bag = class Bag {
         this.bagMarbles.push(marbleDataList[72])
         this.bagMarbles.push(marbleDataList[75])
         this.bagMarbles.push(marbleDataList[78])
-
-    this.bagMarbles.push(marbleDataList[281])
-    this.bagMarbles.push(marbleDataList[391])
-    this.bagMarbles.push(marbleDataList[596])
-    this.bagMarbles.push(marbleDataList[703])
-    this.bagMarbles.push(marbleDataList[704])
-    this.bagMarbles.push(marbleDataList[1032])
-    this.bagMarbles.push(marbleDataList[1045])
-    this.bagMarbles.push(marbleDataList[1046])
-    this.bagMarbles.push(marbleDataList[1743])
-    this.bagMarbles.push(marbleDataList[1746])
-    this.bagMarbles.push(marbleDataList[2722])
-    this.bagMarbles.push(marbleDataList[2723])
-    this.bagMarbles.push(marbleDataList[2724])
     }
 }
